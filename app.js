fbd6578f471d82ce23787fdb1ae5fafd28ea2139
David Schenck <zero2cx@gmail.com>
2017-06-29 00:20:09 +0000

    styling nav and forms [248]

--- app.js
+++ app.js
@@ -3,23 +3,40 @@
 
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+var campgrounds = [
+  {name: "Salmon Creek", image: "https://farm5.staticflickr.com/4153/4835814837_feef6f969b.jpg"},
+  {name: "Granite Hill", image: "https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg"},
+  {name: "Mountain Goat's Rest", image: "https://farm8.staticflickr.com/7338/9627572189_12dbd88ebe.jpg"},
+  {name: "Salmon Creek", image: "https://farm5.staticflickr.com/4153/4835814837_feef6f969b.jpg"},
+  {name: "Granite Hill", image: "https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg"},
+  {name: "Mountain Goat's Rest", image: "https://farm8.staticflickr.com/7338/9627572189_12dbd88ebe.jpg"},
+  {name: "Salmon Creek", image: "https://farm5.staticflickr.com/4153/4835814837_feef6f969b.jpg"},
+  {name: "Granite Hill", image: "https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg"},
+  {name: "Mountain Goat's Rest", image: "https://farm8.staticflickr.com/7338/9627572189_12dbd88ebe.jpg"}
+];
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
-
-app.get("/campgrounds", function(req, res){
-  var campgrounds = [
-    {name: "Salmon Creek", image: "http://www.photosforclass.com/download/4835814837"},
-    {name: "Granite Hill", image: "http://www.photosforclass.com/download/7121863467"},
-    {name: "Mountain Goat's Rest", image: "http://www.photosforclass.com/download/9627572189"}
-  ];
-
-  res.render("campgrounds", {campgrounds: campgrounds});
-});
 
 app.get("/", function(req,res){
   res.render("landing");
 });
 
+app.get("/campgrounds", function(req, res){
+  res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.post("/campgrounds", function(req, res){
+  var name = req.body.name;
+  var image = req.body.image;
+  campgrounds.push({name: name, image: image})
+  res.redirect("/campgrounds");
+});
+
+app.get("/campgrounds/new", function(req, res){
+  res.render("new");
+});
 app.listen(process.env.PORT, process.env.IP, function(){
   console.log("info: server is running");
 });
