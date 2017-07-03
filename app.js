8277e3b5ad512080a30c560b356b7056ed0ebac1
David Schenck <zero2cx@gmail.com>
2017-07-03 06:17:19 +0000

    comment model [275]

--- app.js
+++ app.js
@@ -5,45 +5,25 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var seedDB = require("./seed-db");
 
 mongoose.connect("mongodb://localhost/yelp_camp");
-var campgroundSchema = new mongoose.Schema({
-  name: String,
-  image: String,
-  description: String
-});
-
-var Campground = mongoose.model("Campground", campgroundSchema);
-
-// var campgrounds = [
-//   {name: "Salmon Creek", image: "https://farm5.staticflickr.com/4153/4835814837_feef6f969b.jpg"},
-//   {name: "Granite Hill", image: "https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg"},
-//   {name: "Mountain Goat's Rest", image: "https://farm8.staticflickr.com/7338/9627572189_12dbd88ebe.jpg"},
-// ];
-
-// Campground.create({
-//   name: "Granite Hill",
-//   image: "https://farm8.staticflickr.com/7205/7121863467_eb0aa64193.jpg",
-//   description: "Nice place, if you like to look at granite!"
-//   }, function(err, campground){
-//     if(err){
-//       console.log("error: " + err);
-//     } else {
-//       console.log("created: " + campground);
-//     }
-// });
-
+mongoose.Promise = global.Promise;
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+seedDB();
 
+// route: SERVER_ROOT
 app.get("/", function(req,res){
   res.render("landing");
 });
 
+// route: INDEX
 app.get("/campgrounds", function(req, res){
   Campground.find({}, function(err, campgrounds){
     if(err){
-      console.log("error: " + err);
+      console.log("** error: " + err);
     } else {
       res.render("index", {campgrounds: campgrounds});
     }
@@ -51,33 +31,37 @@ app.get("/campgrounds", function(req, res){
 
 });
 
+// route: NEW
+app.get("/campgrounds/new", function(req, res){
+  res.render("new");
+});
+
+// route: CREATE
 app.post("/campgrounds", function(req, res){
   var name = req.body.name;
   var image = req.body.image;
   var desc = req.body.description;
   Campground.create({name: name, image: image, description: desc}, function(err, campground){
     if(err){
-      console.log("error: " + err);
+      console.log("** error: " + err);
     } else {
       res.redirect("/campgrounds");
     }
   });
 });
 
-app.get("/campgrounds/new", function(req, res){
-  res.render("new");
-});
-
+// route: SHOW
 app.get("/campgrounds/:id", function(req, res){
-  Campground.findById(req.params.id, function(err, campground){
+  Campground.findById(req.params.id).populate("comments").exec(function(err, campground){
     if(err){
-      console.log("error: " + err);
+      console.log("** error: " + err);
     } else {
       res.render("show", {campground: campground});
     }
   });
 });
 
+// listen for incoming requests
 app.listen(process.env.PORT, process.env.IP, function(){
-  console.log("info: server is running");
+  console.log("++ info: server is running");
 });
---
+++ seed-db.js
@@ -0,0 +1,58 @@
+var mongoose = require("mongoose");
+var Campground = require("./models/campground");
+var Comment = require("./models/comment");
+
+var campgroundData = [
+  {
+    name: "Woodsy Place",
+    image: "https://farm8.staticflickr.com/7252/7626464792_3e68c2a6a5.jpg",
+    description: "In the woods, for sure."
+  },
+  {
+    name: "Beach Camp",
+    image: "https://farm4.staticflickr.com/3872/14435096036_39db8f04bc.jpg",
+    description: "On the beach, for sure."
+  },
+  {
+    name: "Picnic Place",
+    image: "https://farm9.staticflickr.com/8456/8006869967_de2ed3e564.jpg",
+    description: "We have picnic tables."
+  }
+]
+
+function seedDB(){
+  // remove data from the db
+  Campground.remove({}, function(err){
+    if(err){
+      console.log("** error: " + err);
+    } else {
+      console.log("++ info: removed db data");
+    }
+    // create new seed data in the db
+    campgroundData.forEach(function(seed){
+      Campground.create(seed, function(err, campground){
+        if(err){
+          console.log("** error: " + err);
+        } else {
+          console.log("++ info: campground created");
+          console.log(campground);
+          Comment.create({
+            text: "What's up, Doc?",
+            author: "Bugs Bunny"
+          }, function(err, comment){
+            if(err){
+              console.log("** error: " + err);
+            } else {
+              campground.comments.push(comment);
+              campground.save();
+              console.log("++ info: comment created");
+              console.log(comment);
+            }
+          });
+        }
+      });
+    });
+  });
+}
+
+module.exports = seedDB;
